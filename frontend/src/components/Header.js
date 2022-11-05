2f598cfb40766dfc8a417eb22d8ca39ba56e97fe
James Li <jameslicarlover@gmail.com>
2022-11-05 14:02:05 -0400

    header draft

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,17 @@
 import '../styles/Header.css';
+import React from 'react';
+import Select from 'react-select';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const aquaticCreatures = [
+  { label: 'Shark', value: 'Shark' },
+  { label: 'Dolphin', value: 'Dolphin' },
+  { label: 'Whale', value: 'Whale' },
+  { label: 'Octopus', value: 'Octopus' },
+  { label: 'Crab', value: 'Crab' },
+  { label: 'Lobster', value: 'Lobster' },
+];
 
 export default function Header() {
     return (
@@ -7,7 +20,12 @@ export default function Header() {
 
             </div>
             <div className="search">
-            
+                <Select
+                    options={aquaticCreatures}
+                />
+            </div>
+            <div className="profile">
+                <FontAwesomeIcon icon={faUser}/>
             </div>
         </div>
     )
