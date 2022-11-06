8e96912a8a7a1d1409dbb4c81e5e4aefa4c8ca5e
Sarah Liu <sarahyeliu@gmail.com>
2022-11-06 03:13:14 -0500

    Fixed chromedriver finds prices of loblaws

--- backend/demo_test.py
+++ backend/demo_test.py
@@ -16,9 +16,25 @@ time.sleep(5)
 #last = driver.find_element_by_xpath('//*[@id="kw"]')
 driver.find_element("xpath", '//*[@id="autocomplete-listbox-desktop-site-header-"]').send_keys('oranges')
 driver.find_element("xpath", '//*[@id="site-layout"]/div[1]/div[3]/div/header/div[1]/div[2]/form/button').click();
+time.sleep(5)
 
 #get the value
-driver.find_element("xpath", '//*[@id="odd"]/div/div/div[3]/div[1]/div/div[3]/div/div/span/span[1]')
+#driver.find_element(By.CLASS_NAME, "product-tracking")
+element = driver.find_elements_by_class_name("product-name__item--name")
+itemNames = []
+for item in element:
+	itemNames.append(item.text)
+print(itemNames)
+
+element = driver.find_elements_by_class_name("selling-price-list__item__price--now-price__value")
+itemPrices = []
+for item in element:
+	itemPrices.append(item.text)
+print(itemPrices)
+
+if(len(itemNames) == len(itemPrices)):
+	print("good!")
+
 
 # clickable = driver.find_element(By.id("clickable"));
 # clickable.click()
