99e6c01a296ef0462a7a3224851b3a0042780c99
Sarah Liu <sarahyeliu@gmail.com>
2022-11-06 01:07:37 -0500

    Added hardCode responses for 4 stores and 3 fruits

--- backend/testscrape.py
+++ backend/testscrape.py
@@ -1,7 +1,19 @@
 import requests
- 
-# Lat-Lon of New York
-URL = "https://www.tntsupermarket.com/eng/t-t-brand/food-essential.html"
+from lxml import etree
+import pandas as pd
+
+# testing getting data from a website
+URL = "https://weather.com/weather/today/l/40.75,-73.98"
+#headers = {'Origin': 'https://www.farmboy.ca/'}
 resp = requests.get(URL)
-print(resp.status_code)
-print(resp.text)
+#if resp.status_code == 200:
+#    data = resp.json()
+#    print(data)
+#print(resp.status_code)
+#print(resp.text)
+
+# Create DOM from HTML text
+dom = etree.HTML(resp.text)
+# Search for the temperature element and get the content
+elements = dom.xpath("//div[@data-testid='wxPhrase' and contains(@class,'CurrentConditions')]")
+print(elements[0].text)
